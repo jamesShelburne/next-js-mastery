2cbb46c07a3c3595f68a02d3e481dc8a0873d676
James Shelburne <jshelburne0@gmail.com>
2024-07-12 00:22:55 -0700

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
+import {useTheme} from '@/context/ThemeProvider'
 import React from "react";
 
 export default function Home() {
+  const {mode} = useTheme();
   return (
     <div>
       <h1>Home</h1>
+      {mode}
       <UserButton afterSignOutUrl= "/"/>
     </div>
   );
