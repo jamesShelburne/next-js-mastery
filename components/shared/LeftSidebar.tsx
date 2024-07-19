25fa86e30ac459a7edc4b47f17e73d8b6c2fd6cb
James Shelburne <jshelburne0@gmail.com>
2024-07-18 23:50:23 -0700

    sidebar and mobile nav

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+
+    <section className = "background-light900_dark200 light-border shadow-light-300 custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-[266px] dark:shadow-none"> 
+    </section>
+  )
+}
+
+export default LeftSidebar
